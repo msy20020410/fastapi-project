2b90956f82703b2fbe54ae747f652a1fa689e93f
mashiyu <dj8817117@gmail.com>
2026-09-06 22:34:01 +0800

    移除授权相关功能

--- app/core/config.py
+++ app/core/config.py
@@ -15,10 +15,5 @@ class Settings(BaseSettings):
     version: str = "0.1.0"
     api_v1_prefix: str = "/api/v1"
 
-    # JWT 安全配置
-    secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
-
 
 settings = Settings()
